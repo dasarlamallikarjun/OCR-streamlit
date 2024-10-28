999ffbe376fc9d3023a8dbf47b337e22265c8f64
dasarlamallikarjun <dasarlamallikarjun@gmail.com>
2024-10-27 22:01:14 -0400

    Initial

--- PDF_OCR.py
+++ PDF_OCR.py
@@ -1,8 +1,6 @@
-import fitz  # PyMuPDF
+from pdf2image import convert_from_path
 import pytesseract
-from PIL import Image  # Import Pillow
 from docx import Document
-import io
 import os
 
 class PDF_OCR:
@@ -11,23 +9,16 @@ class PDF_OCR:
         self.lang = lang  # Set language for OCR
 
     def process_pdf(self):
-        # Open PDF with PyMuPDF
-        pdf_document = fitz.open(self.pdf_path)
-        
+        # Convert PDF to images
+        pages = convert_from_path(self.pdf_path)
+
         # Create a new Word document
         doc = Document()
 
-        # Process each page in the PDF
-        for page_number in range(len(pdf_document)):
-            # Render page to an image
-            page = pdf_document.load_page(page_number)
-            pix = page.get_pixmap()
-            
-            # Convert Pixmap to Pillow image
-            image = Image.open(io.BytesIO(pix.tobytes("png")))
-
-            # Perform OCR on the image
-            text = pytesseract.image_to_string(image, lang=self.lang)
+        # Perform OCR on each page and add text to the DOCX document
+        for page in pages:
+            # Convert the image to a format that pytesseract can use
+            text = pytesseract.image_to_string(page, lang=self.lang)
             doc.add_paragraph(text)
             doc.add_page_break()  # Add a page break after each page
 
